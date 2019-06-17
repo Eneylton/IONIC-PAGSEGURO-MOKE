844413c14b01abbfdeb98cc9a66a5a981549cb2c
eneylton <eneylton@hotmail.com>
2019-06-16 23:19:38 -0300

    Projeto IONIC PAGUESEGURO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,34 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
+import {HttpModule} from "@angular/http";
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ProductHttpProvider } from '../providers/product-http/product-http';
+import { PaymentHttpProvider } from '../providers/payment-http/payment-http';
+import { CartProvider } from '../providers/cart/cart';
+
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
-    ListPage
+    MyApp
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ProductHttpProvider,
+    PaymentHttpProvider,
+    CartProvider
   ]
 })
-export class AppModule {}
+export class AppModule { }
